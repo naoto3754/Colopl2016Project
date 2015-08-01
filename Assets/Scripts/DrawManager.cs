9acf8198fb1c2f86fc572f860b8279075dc1dc6b
Naoto Sasaki <naoto3754@gmail.com>
2015-08-02 00:50:23 +0900

    速くマウスを動かしても間が空かないように修正（オブジェクト大量生成しますが、お絵描きできます）

--- Assets/Scripts/DrawManager.cs
+++ Assets/Scripts/DrawManager.cs
@@ -8,25 +8,35 @@ public class DrawManager : MonoBehaviour
 	[SerializeField]
 	private float _DrawIntervalScale;
 
-	
+	private float _DrawInterval;
 	private Plane _DrawPlane;
 	private Vector3 _PreviousDrawPosition;
 	
 	void Start()
 	{
-		_DrawPlane = new Plane(new Vector3(0f,0f,0f), new Vector3(-1f,1f,0f), new Vector3(1f,1f,0f));//(Vector3.back, Vector3.zero);
-
+		_DrawInterval = _DrawUnitPrefab.transform.localScale.x*_DrawIntervalScale;
+		_DrawPlane = new Plane(new Vector3(0f,0f,0f), new Vector3(-1f,1f,0f), new Vector3(1f,1f,0f));
 	}
 	
 	void Update()
 	{
-		if(InputManager.I.GetTap())
+		if(InputManager.I.GetTapDown())
 		{
 			Vector3 CurrentPosition = InputManager.I.GetTappedPointAcrossPlane(_DrawPlane);
-			if(Vector3.Distance(CurrentPosition, _PreviousDrawPosition)
-				> _DrawUnitPrefab.transform.localScale.x*_DrawIntervalScale)
+			_PreviousDrawPosition = CurrentPosition;
+		}
+		else if(InputManager.I.GetTap())
+		{
+			Vector3 CurrentPosition = InputManager.I.GetTappedPointAcrossPlane(_DrawPlane);
+			float distance = Vector3.Distance(CurrentPosition, _PreviousDrawPosition); 
+			if(distance > _DrawInterval)
 			{
-				Instantiate(_DrawUnitPrefab, CurrentPosition, Quaternion.identity);
+				int loopCnt = (int)(distance / _DrawInterval);
+				for(int i = 0; i < loopCnt; i++){
+					Instantiate(_DrawUnitPrefab,
+								Vector3.Lerp(_PreviousDrawPosition, CurrentPosition, (float)i/loopCnt),
+								Quaternion.identity);
+				}
 				_PreviousDrawPosition = CurrentPosition;
 			}
 		}	
