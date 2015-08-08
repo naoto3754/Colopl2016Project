9715a71328f8cc1368e481fe875ae03c2ff7b2ba
Naoto Sasaki <naoto3754@gmail.com>
2015-08-08 22:02:16 +0900

    床で爆発するように

--- Assets/Developers/Sasaki/BombFlow.cs
+++ Assets/Developers/Sasaki/BombFlow.cs
@@ -17,19 +17,19 @@ public class BombFlow : MonoBehaviour {
 	}
 	
 	void OnTriggerEnter(Collider col){
-		GameObject _ParentObject = new GameObject("DrawUnitParent");
-		_ParentObject.layer = LayerMask.NameToLayer("DrawObject");
-		Rigidbody rig = _ParentObject.AddComponent<Rigidbody>();
-		rig.constraints = RigidbodyConstraints.FreezeRotationX| 
-							  RigidbodyConstraints.FreezeRotationY| 
-							  RigidbodyConstraints.FreezePositionZ;
-		col.gameObject.transform.parent = _ParentObject.transform;
-		col.gameObject.transform.parent.GetComponent<Rigidbody>().AddForce(0.001f*(col.gameObject.transform.position - transform.position), ForceMode.Impulse);
+		if(col.gameObject.layer != LayerMask.NameToLayer("Floor")){
+			GameObject _ParentObject = new GameObject("DrawUnitParent");
+			_ParentObject.layer = LayerMask.NameToLayer("DrawObject");
+			Rigidbody rig = _ParentObject.AddComponent<Rigidbody>();
+			rig.constraints = RigidbodyConstraints.FreezeRotationX| 
+								  RigidbodyConstraints.FreezeRotationY| 
+								  RigidbodyConstraints.FreezePositionZ;
+			col.gameObject.transform.parent = _ParentObject.transform;
+			col.gameObject.transform.parent.GetComponent<Rigidbody>().AddForce(0.001f*(col.gameObject.transform.position - transform.position), ForceMode.Impulse);
+			Destroy(_ParentObject, 2.0f);
+		}
 		
-		Debug.Log("instantiate");
 		Instantiate(effect, transform.parent.position, Quaternion.identity);
-		
-		Destroy(_ParentObject, 2.0f);
 		Destroy(transform.parent.gameObject);
 	}
 }
