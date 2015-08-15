5c3a2af3932a3425a788cf4fbb00f5c2b0143c66
Naoto Sasaki <naoto3754@gmail.com>
2015-08-15 12:46:34 +0900

    ポップアップカードの壁面を動くように

---
+++ Assets/Scripts/PopupCard/CharacterController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+	[SerializeField]
+	private GameObject _Character;
+	[SerializeField]
+	private GameObject _DummyCharacter;
+	[SerializeField]
+	private DummyCard _DummyCard;
+	[SerializeField]
+	private float _Speed;
+	
+	void Update () {
+		float delta = _Speed * Input.GetAxis("Horizontal");
+		
+		if(_DummyCard.MoveXaxis(_DummyCharacter.transform.position))
+		{
+			_Character.transform.Translate(delta, 0f, 0f);
+		}
+		else
+		{
+			_Character.transform.Translate(0f, 0f, -delta);
+		}
+		
+		_DummyCharacter.transform.Translate(delta, 0f, 0f);
+	}
+}
---
+++ Assets/Scripts/PopupCard/DummyCard.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DummyCard : MonoBehaviour {
+	private List<Rect> _CardRects;
+	
+	void Awake () {
+		_CardRects = new List<Rect>();
+		foreach(Transform child in transform)
+		{
+			_CardRects.Add(new Rect(child.localPosition.x - child.lossyScale.x/2, child.localPosition.y - child.lossyScale.y/2,
+									child.lossyScale.x, child.lossyScale.y));
+		}
+	}
+	
+	public bool MoveXaxis(Vector3 charaPos)
+	{
+		bool ret = true;
+		if(charaPos.x > 0f)
+			ret = !ret;
+		foreach(Rect rect in _CardRects)
+		{
+			if(rect.Contains(charaPos)){
+				ret = !ret;
+			}
+		}
+		return ret;
+	}
+}
+
+//  public class CardRect
+//  {
+//  	public float center;
+//  	public float width;
+//  	public float height;
+	
+//  	public CardRect(float center, float width, float height)
+//  	{
+//  		this.center = center;
+//  		this.width = width;
+//  		this.height = height;
+//  	} 
+//  }
