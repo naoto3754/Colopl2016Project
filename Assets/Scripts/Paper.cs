5af3ebe33f90bb87c7f27276082b05b726c78623
Naoto Sasaki <naoto3754@gmail.com>
2015-08-23 15:17:23 +0900

    紙テクスチャ導入

---
+++ Assets/Scripts/Paper.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paper : MonoBehaviour {
+
+	void Start () {
+		Material paperMaterial = gameObject.GetComponent<Renderer>().material;
+		paperMaterial.mainTextureScale = transform.localScale*0.15f;	
+	}
+}
