eecf9c07b7a0795505ddb923a6cb3310c196f369
Naoto Sasaki <naoto3754@gmail.com>
2015-09-26 23:13:40 +0900

    DoTweenExtension追加

---
+++ Assets/Scripts/Util/DoTweenExtension.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.ImageEffects;
+using DG.Tweening;
+
+public static class DoTweenExtension
+{
+	public static Tween DOBlurSize(this BlurOptimized target, float endValue, float duration)
+	{
+		return DOTween.To(()=>target.blurSize , x=>target.blurSize=x, endValue, duration );
+	}
+}
