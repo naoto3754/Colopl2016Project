acce3059f5a6f6be63ce03e1c4f9460a7312aa39
Naoto Sasaki <naoto3754@gmail.com>
2015-09-26 23:13:58 +0900

    ステージに入るときのブラー実装

--- Assets/Scripts/System/InGameManager.cs
+++ Assets/Scripts/System/InGameManager.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityStandardAssets.ImageEffects;
+using DG.Tweening;
 
 public class InGameManager : Singlton<InGameManager> 
 {
@@ -8,6 +9,18 @@ public class InGameManager : Singlton<InGameManager>
 	{
 		BlurOptimized blur = Camera.main.GetComponent<BlurOptimized>();
 		blur.enabled = true;
-		StageCreater.I.IsPlayingAnimation = false;
+		Sequence seq = DOTween.Sequence();
+		//フェードイン
+		seq.Append( blur.DOBlurSize(3f, 1f).SetEase(Ease.OutSine) );
+		//UI表示
+		//フェードアウト
+		seq.Append( blur.DOBlurSize(0f, 1f).SetEase(Ease.InSine) );
+		//終了処理
+		seq.OnComplete(() => 
+		{
+			blur.enabled = false;
+			StageCreater.I.IsPlayingAnimation = false;
+		});
+		seq.Play();
 	}
 }
